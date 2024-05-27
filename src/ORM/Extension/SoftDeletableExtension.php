95a679d758484114c99c2b367a9f449983ca75c9
Gustavo Henrique Mascarenhas Machado <guhemama@gmail.com>
2024-05-27 11:24:24 +0200

    Add to single intem

--- src/ORM/Extension/SoftDeletableExtension.php
+++ src/ORM/Extension/SoftDeletableExtension.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace App\ORM\Extension;
 
 use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Metadata\Operation;
 use App\Entity\Kunde;
 use Doctrine\ORM\QueryBuilder;
 
-class SoftDeletableExtension implements QueryCollectionExtensionInterface
+class SoftDeletableExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
 {
 
     public function applyToCollection(
@@ -21,9 +22,28 @@ class SoftDeletableExtension implements QueryCollectionExtensionInterface
         array $context = []
     ): void
     {
-        if (Kunde::class === $resourceClass) {
-            $rootAlias = $queryBuilder->getRootAliases()[0];
-            $queryBuilder->andWhere(sprintf('%s.geloescht = 0', $rootAlias));
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    public function applyToItem(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        array $identifiers,
+        Operation $operation = null,
+        array $context = []
+    ): void
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass): void
+    {
+        if (Kunde::class !== $resourceClass) {
+            return;
         }
+        
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder->andWhere(sprintf('%s.geloescht = 0', $rootAlias));
     }
 }
