cace72237c8849fbffcfe239be5f623f10227acf
Thoralf Schillat <thoralf.schillat@check24.de>
2024-05-29 15:32:21 +0200

    Add deleted user authentication test

--- tests/Controller/AuthenticationTest.php
+++ tests/Controller/AuthenticationTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Controller;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthenticationTest extends ApiTestCase
 {
@@ -11,5 +12,22 @@ class AuthenticationTest extends ApiTestCase
         $client = static::createClient();
         $client->request('POST', '/foo');
         self::assertResponseIsSuccessful();
+    }
+
+    public function testDeletedVermittlerUserCantLogin(): void
+    {
+        static::createClient()->request(
+            method: 'POST',
+            url: '/login',
+            options: [
+                'json' => [
+                    'username' => 'vermittler_vincent_vincent@email.com',
+                    'password' => 'hackme',
+                ],
+            ]
+        );
+
+        self::assertResponseStatusCodeSame(Response::HTTP_UNAUTHORIZED);
+        self::assertJson('{"code":401,"message":"Der Account existiert nicht mehr."}');
     }
 }
