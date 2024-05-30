6151dad58c15519af89b44b48dcf27d256bd4bad
Thoralf Schillat <thoralf.schillat@check24.de>
2024-05-30 20:54:02 +0200

    [FOO-8] implement address details
    Leider mit Hack. Ich würde mich über Feedback zu diesem Problem im PR freuen.

--- src/Entity/KundeAdresse.php
+++ src/Entity/KundeAdresse.php
@@ -2,9 +2,35 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Link;
+use App\Extension\ApiPlatform\State\KundeAdresseProvider;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 
+#[ApiResource(
+    shortName: 'Kunden',
+    operations: [
+        new Get(
+            uriTemplate: '/kunden/{kundeId}/adressen/{adresseId}/details',
+            uriVariables: [
+                'kundeId' => new Link(
+                    fromProperty: 'adressen',
+                    fromClass: Kunde::class,
+                ),
+                'adresseId' => new Link(
+                    fromProperty: 'kundeAdresse',
+                    fromClass: Adresse::class,
+                ),
+            ],
+            provider: KundeAdresseProvider::class,
+        ),
+    ],
+    normalizationContext: ['groups' => ['kunde_adresse:read']],
+    denormalizationContext: ['groups' => ['kunde_adresse:write']],
+    security: 'is_granted("ROLE_VERMITTLER")'
+)]
 #[ORM\Entity]
 #[ORM\Table(name: 'kunde_adresse', schema: 'std')]
 class KundeAdresse
@@ -21,11 +47,11 @@ class KundeAdresse
     #[ORM\GeneratedValue(strategy: 'NONE')]
     public Adresse $adresse;
 
-    #[Groups(['adresse:read', 'kunde:read'])]
+    #[Groups(['kunde_adresse:read', 'adresse:read', 'kunde:read'])]
     #[ORM\Column(options: ['default' => false])]
     public bool $geschaeftlich = false;
 
-    #[Groups(['adresse:read', 'kunde:read'])]
+    #[Groups(['kunde_adresse:read', 'adresse:read', 'kunde:read'])]
     #[ORM\Column]
     public bool $rechnungsadresse;
 
